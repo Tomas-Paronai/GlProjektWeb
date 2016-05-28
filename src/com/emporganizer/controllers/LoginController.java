62ce4a9131b0ffbea09d4d9b70960a79b6a41fcb
Tomáš Paronai <tomas.parok@gmail.com>
2016-05-28 12:18:29 +0200

    build

---
+++ src/com/emporganizer/controllers/LoginController.java
@@ -0,0 +1,15 @@
+package com.emporganizer.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class LoginController {
+	
+	@RequestMapping("/home")
+	public ModelAndView getHomePage(){
+		ModelAndView model = new ModelAndView("home");
+		return model;
+	}
+}
